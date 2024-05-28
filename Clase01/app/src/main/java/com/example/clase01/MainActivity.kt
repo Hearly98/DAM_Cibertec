6b2a7b6728bafab079b4398ad8cb12a20420ef23
unknown <SUITE@SUITECE323-ST09.cibertec.edu.pe>
2024-05-27 20:50:47 -0500

    Agregando un evento cliqueable al label

--- Clase01/app/src/main/java/com/example/clase01/MainActivity.kt
+++ Clase01/app/src/main/java/com/example/clase01/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.clase01
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1ra manera de contener en la variable un id
+        // 2da manera  var tvejemplo: TextView = findViewById(R.id.tvejemplo)
+         var tvejemplo = findViewById<TextView>(R.id.tvejemplo)
+
+        tvejemplo.text = "Texto desde Backend de Kotlin"
+        tvejemplo.setTextColor(Color.RED)
+        tvejemplo.setTypeface(Typeface.MONOSPACE, Typeface.BOLD)
+
+        // Evento para el label
+        tvejemplo.setOnClickListener{
+            Toast.makeText(this, "TextView cliqueado", Toast.LENGTH_SHORT).show()
+            tvejemplo.setTextColor(Color.GREEN)
+        }
     }
 }
